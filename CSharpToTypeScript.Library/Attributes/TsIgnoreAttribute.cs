3a4d5662be31d7bf31de9779672be44c98626f33
Klas Tärnström <klas.tarnstrom@alstra.se>
2025-01-08 08:48:28 +0100

    Enable TsIgnore on properties and fields

--- CSharpToTypeScript.Library/Attributes/TsIgnoreAttribute.cs
+++ CSharpToTypeScript.Library/Attributes/TsIgnoreAttribute.cs
@@ -1,4 +1,5 @@
 namespace CSharpToTypeScript.Library.Attributes;
 
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum | AttributeTargets.Interface)]
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum | AttributeTargets.Interface |
+                AttributeTargets.Property | AttributeTargets.Field)]
 public class TsIgnoreAttribute : Attribute;
