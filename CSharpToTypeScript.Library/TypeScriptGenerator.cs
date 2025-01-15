b7e9227a898ef3b0871bde9ef46ee7e8bf3d8158
Klas Tärnström <klas.tarnstrom@alstra.se>
2025-01-15 13:36:59 +0100

    Get assemblies as argument

--- CSharpToTypeScript.Library/TypeScriptGenerator.cs
+++ CSharpToTypeScript.Library/TypeScriptGenerator.cs
@@ -1,13 +1,13 @@
+using System.Reflection;
+
 namespace CSharpToTypeScript.Library;
 
 public class TypeScriptGenerator(TypeScriptConfiguration? configuration = null)
 {
     private readonly TypeScriptConfiguration _configuration = configuration ?? new();
 
-    public async Task Generate()
+    public async Task Generate(params Assembly[] assemblies)
     {
-        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-
         await using var writer = new TypeScriptWriter(_configuration);
         
         // Collect all types from the assemblies that have the TsGenerate attribute as well as all nested types
--- CsharpToTypeScript/Program.cs
+++ CsharpToTypeScript/Program.cs
@@ -1,5 +1,6 @@
-﻿using CSharpToTypeScript.Library;
+﻿using CSharpToTypeScript.ExampleData;
+using CSharpToTypeScript.Library;
 
 var configuration = new TypeScriptConfiguration();
 
-await new TypeScriptGenerator(configuration).Generate();
+await new TypeScriptGenerator(configuration).Generate(typeof(TestClass).Assembly);
